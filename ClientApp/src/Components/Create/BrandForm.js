7c413e4ebf262bf6ea3ce974364100e52115abbb
Augustina <37456430+Naktis@users.noreply.github.com>
2021-10-25 21:59:02 +0300

    Add brand form validation

--- ClientApp/src/Components/Create/BrandForm.js
+++ ClientApp/src/Components/Create/BrandForm.js
@@ -2,53 +2,31 @@ import React from "react";
 import axios from "axios";
 import { components, default as ReactSelect } from "react-select";
 import { Redirect } from 'react-router-dom'
-import FormValid from "./FormValid";
-
-const Option = (props) => {
-  return (
-    <div>
-      <components.Option {...props}>
-        <input
-          type="checkbox"
-          checked={props.isSelected}
-          onChange={() => null}
-        />{" "}
-        <label>{props.label}</label>
-      </components.Option>
-    </div>
-  );
-};
 
 class BrandForm extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-      categories: [],
       companies: [],
-      name: this.props.brand.name,
-      description: this.props.brand.description,
-      companyId: this.props.brand.companyId,
       categoryOptions: [],
-      optionSelected: null,
-      createSucessful: false
+      createSucessful: false,
+      fields: {
+        "name": "", 
+        "description":"", 
+        "companyId": 0, 
+        "categories": null
+      },
+      errors: {}
 		};
 
-    this.handleCategoryChange = this.handleCategoryChange.bind(this);
-    this.handleCompanyChange = this.handleCompanyChange.bind(this);
     this.collectData = this.collectData.bind(this);
-    this.handleNameChange = this.handleNameChange.bind(this);
-    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
 	}
 
   componentDidMount() {
     const _this = this;
-    //_this.state.brand = _this.props.brand;
 
     axios.get("https://localhost:5001/api/Category").then(function(response) {
-      _this.setState({
-        categories: response.data
-      })
-      _this.setOptions(response.data)
+      _this.setCategories(response.data)
       }).catch((error) => {
         console.log(error);
     })
@@ -62,7 +40,7 @@ class BrandForm extends React.Component {
     })
   }
 
-  setOptions(categories) {
+  setCategories(categories) {
     let options = []
     categories.forEach(c => {
       options.push({
@@ -73,82 +51,118 @@ class BrandForm extends React.Component {
     this.setState({categoryOptions: options}); 
   }
 
-  handleCategoryChange(event) {    
-    this.setState({categoryId: event.target.value});  
+  handleChange(field, e) {
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({ fields });
   }
 
-  handleCompanyChange(event) {    
-    this.setState({companyId: event.target.value});  
-  }
-
-  handleNameChange(event) {    
-    this.setState({
-      name: event.target.value
-    });  
-  }
-
-  handleDescriptionChange(event) {    
-    this.setState({
-      description: event.target.value
-    });  
-  }
-
-  handleOptionsChange = (selected) => {
-    this.setState({
-      optionSelected: selected
-    });
+  handleCategoryChange = (selected) => {
+    let fields = this.state.fields;
+    fields["categories"] = selected;
+    this.setState({ fields });
   };
+
+  handleValidation() {
+    let formValid = true;
+    let errors = {};
+    let fields = this.state.fields;
+
+    if (!fields["name"]) {
+      formValid = false;
+      errors["name"] = "Įveskite pavadinimą";
+    } else if (fields["name"].length < 2) {
+      formValid = false;
+      errors["name"] = "Pavadinimą turi sudaryti daugiau nei 1 simbolis";
+    }
+
+    if (!fields["description"]) {
+      formValid = false;
+      errors["description"] = "Įveskite aprašymą";
+    } else if  (fields["description"].length < 10) {
+      formValid = false;
+      errors["description"] = "Aprašymą turi sudaryti daugiau nei 10 simbolių";
+    }
+
+    if (fields["companyId"] === 0) {
+      formValid = false;
+      errors["companyId"] = "Pasirinkite įmonę iš sąrašo";
+    }
+
+    this.setState({ errors: errors });
+    return formValid;
+  }
 
   collectData(e) {
     e.preventDefault();
-    let categoryIds = [];
 
-    if (this.state.optionSelected !== null)
-      this.state.optionSelected.forEach(o => {
-        categoryIds.push(o.value);
-      })
+    if (!this.handleValidation())
+      return;
 
-    let newBrand = {
-      name: this.state.name,
-      description: this.state.description,
-      companyId: this.state.companyId,
-      categoryIds: categoryIds
-    }
+    let fields = this.state.fields;
 
-    if (FormValid(newBrand)) {
-      this.props.handleSubmit(newBrand);
-      this.setState({createSucessful: true});
-    }
+    fields["categoryIds"] = [];
+    if (fields["categories"] !== null)
+      fields["categories"].forEach(o => {
+        fields["categoryIds"].push(o.value);
+    })
+    delete fields["categories"];
+
+    this.props.handleSubmit(fields);
+    this.setState({createSucessful: true});
+
   }
 
 	render() {
 		return(
 			<form className="New-brand-form">
         <h1>{this.props.title}</h1>
+
         <label>Pavadinimas:</label>
-        <input type="text" value={this.state.name} onChange={this.handleNameChange} required minLength="2" maxLength="40"/>
-        <label>Aprašymas</label><textarea value={this.state.description} onChange={this.handleDescriptionChange} required minLength="5" maxLength="200"/>
+        <input 
+          type="text" 
+          value={this.state.fields["name"]}
+          onChange={this.handleChange.bind(this, "name")}
+          maxLength="40"
+        />
+        <span style={{ color: "red" }}>{this.state.errors["name"]}</span>
+
+        <label>Aprašymas</label>
+        <textarea 
+          value={this.state.fields["description"]}
+          onChange={this.handleChange.bind(this, "description")}
+          required 
+          minLength="5" 
+          maxLength="200"
+        />
+        <span style={{ color: "red" }}>{this.state.errors["description"]}</span>
+
         <label>Kategorija</label>
         <ReactSelect
           options={this.state.categoryOptions}
           isMulti
           closeMenuOnSelect={false}
           hideSelectedOptions={false}
-          components={{
-            Option
-          }}
-          onChange={this.handleOptionsChange}
+          components={Categories}
+          onChange={this.handleCategoryChange}
           allowSelectAll={true}
-          value={this.state.optionSelected}
-          placeholder="Pasirinkite kategoriją"
+          value={this.state.fields["categories"]}
+          placeholder="Pasirinkite kategorijas"
         />
+
         <label>Įmonė</label>
-        <select value={this.state.companyId} onChange={this.handleCompanyChange} required="required">
+        <select 
+          value={this.state.fields["companyId"]} 
+          onChange={this.handleChange.bind(this, "companyId")} 
+          required="required"
+        >
         <option value="0" disabled={true}>Pasirinkite įmonę</option>
           { this.state.companies.map(function (company){
               return <option value={company.companyId} key={company.companyId}>{company.name}</option>
           }) }
         </select>
+        <span style={{ color: "red" }}>{this.state.errors["companyId"]}</span>
+
         <input type="submit" value="KURTI" onClick={ this.collectData }/>
         {this.state.createSucessful ? <Redirect to="/" /> : <div></div>}
       </form>
@@ -157,3 +171,19 @@ class BrandForm extends React.Component {
 }
 
 export default BrandForm;
+
+
+const Categories = (props) => {
+  return (
+    <div>
+      <components.Option {...props}>
+        <input
+          type="checkbox"
+          checked={props.isSelected}
+          onChange={() => null}
+        />{" "}
+        <label>{props.label}</label>
+      </components.Option>
+    </div>
+  );
+};
